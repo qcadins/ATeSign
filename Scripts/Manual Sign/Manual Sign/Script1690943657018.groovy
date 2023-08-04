015059d7b39e44c82fe17b6f2b8db4f5adf59ff5
wiky.hendra <wiky.hendra@09-05-0045-0123.ad-ins.com>
2023-08-04 15:41:13 +0700

    ATEENDIGO - 172 Push

--- Scripts/Manual Sign/Manual Sign/Script1690943657018.groovy
+++ Scripts/Manual Sign/Manual Sign/Script1690943657018.groovy
@@ -1,7 +1,5 @@
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
