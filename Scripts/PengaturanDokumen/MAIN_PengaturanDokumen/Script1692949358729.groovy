0e6e0890178cc7b9513fd55b86002c6af2a16b43
fendy.tio <fendy.tio@09-05-0040-0222.ad-ins.com>
2023-09-08 09:46:24 +0700

    ATEENDIGO-229

--- Scripts/PengaturanDokumen/MAIN_PengaturanDokumen/Script1692949358729.groovy
+++ Scripts/PengaturanDokumen/MAIN_PengaturanDokumen/Script1692949358729.groovy
@@ -1,6 +1,8 @@
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling
 import com.kms.katalon.core.testobject.TestObject
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
