add930cae2e0cf247027f8af94ca613ff1b07d70
wiky.hendra <wiky.hendra@09-05-0045-0123.ad-ins.com>
2023-09-14 17:23:32 +0700

    ATEENDIGO - Request Dev

---
+++ Object Repository/Postman/Sent Otp Signing Embed.rs
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>Sent Otp Signing Embed</name>
+   <tag></tag>
+   <elementGuidId>ce40964c-41d5-4149-8762-955887336873</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n\n    \&quot;msg\&quot;: \&quot;Pvkay+O8L4Wg/1CNGMLhW/S+yC6Ea0VnV5HuBhiqZ58OLQ34hshCPd3NbGINegR45hOdKC3Q8vWlMS0VGQnS1Q\u003d\u003d\&quot;,\n\n    \&quot;tenantCode\&quot;: \&quot;WOMF\&quot;,\n\n    \&quot;vendorCode\&quot;: \&quot;VIDA\&quot;,\n\n    \&quot;phoneNo\&quot;: \&quot;0089654990285\&quot;,\n\n    \&quot;audit\&quot;: {\n\n        \&quot;callerId\&quot;: \&quot;ANDY-CHECK\&quot;\n\n    }\n\n}&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+      <webElementGuid>950fc6de-7c1c-4cdf-ada6-81ed28e9f4f1</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Accept</name>
+      <type>Main</type>
+      <value>application/json</value>
+      <webElementGuid>ede9ed15-2035-407d-b5ac-d62615e017bc</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Cookie</name>
+      <type>Main</type>
+      <value>idjs=C09022BF63CF2BDECCAC9AC0C04F8EB0</value>
+      <webElementGuid>a7edb4d8-054e-4c98-b47c-25005d7fce70</webElementGuid>
+   </httpHeaderProperties>
+   <katalonVersion>8.5.5</katalonVersion>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>http://gdkwebsvr:7021/adimobile/esign/services/embed/user/sentOtpSigningEmbed</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Object Repository/Postman/Verify Otp Signing Embed.rs
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>Verify Otp Signing Embed</name>
+   <tag></tag>
+   <elementGuidId>a75d2a6a-e086-4f00-9c3a-9f7a0b24acee</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n\n \n\n    \&quot;msg\&quot;: \&quot;Pvkay+O8L4Wg/1CNGMLhWwd6zTnUmyJhSCBNZh5Y63fxT7sOQxTm2rB3eZYTe52fJoIeygMYaeQaUttJvPq/Aw\u003d\u003d\&quot;,\n\n \n\n    \&quot;tenantCode\&quot;: \&quot;WOMF\&quot;,\n\n \n\n    \&quot;phoneNo\&quot;: \&quot;081233444034\&quot;,\n\n \n\n    \&quot;otpCode\&quot;: \&quot;\&quot;,\n\n \n\n    \&quot;audit\&quot;: {\n\n \n\n        \&quot;callerId\&quot;: \&quot;ANDY-CHECK\&quot;\n\n \n\n    }\n\n \n\n}&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+      <webElementGuid>ef6bab64-8d36-42de-b099-e7e5d2e9358d</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Accept</name>
+      <type>Main</type>
+      <value>application/json</value>
+      <webElementGuid>688e769e-e5d5-421f-8849-809f832a88db</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Cookie</name>
+      <type>Main</type>
+      <value>idjs=C09022BF63CF2BDECCAC9AC0C04F8EB0; idjs=DC6A16535DC32F0B79F5C9FF892FDA01</value>
+      <webElementGuid>04de75a9-2d29-4c33-855a-8eea5a8f12fd</webElementGuid>
+   </httpHeaderProperties>
+   <katalonVersion>8.5.5</katalonVersion>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>http://gdkwebsvr:7021/adimobile/esign/services/embed/user/verifyOtpSigningEmbed</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
