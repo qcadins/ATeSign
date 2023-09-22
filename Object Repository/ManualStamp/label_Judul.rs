80a03f1ffd620b33f4b228c55a26bff6f6dfe8b5
fendy.tio <fendy.tio@09-05-0040-0222.ad-ins.com>
2023-09-22 17:15:03 +0700

    ATEENDIGO-240

---
+++ Object Repository/ManualStamp/label_Judul.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>label_Judul</name>
+   <tag></tag>
+   <elementGuidId>f92a02eb-b05a-4aee-8a5e-4f06846bf85c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@class = 'content-header']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>content-header</value>
+      <webElementGuid>4d4d978d-b5b0-4645-9e73-57cfdf17b833</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
