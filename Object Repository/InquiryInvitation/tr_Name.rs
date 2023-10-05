3a4c9000712e7df5fa0b4dfeac3413b38e63be90
fendy.tio <fendy.tio@09-05-0040-0222.ad-ins.com>
2023-10-05 17:26:54 +0700

    merge

--- Object Repository/InquiryInvitation/tr_Name.rs
+++ Object Repository/InquiryInvitation/tr_Name.rs
@@ -6,6 +6,10 @@
    <elementGuidId>06589174-d3f8-4b00-a081-39d6ee4a1ad9</elementGuidId>
    <selectorCollection>
       <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
          <key>BASIC</key>
          <value>//*[@class = 'datatable-row-center datatable-row-group ng-star-inserted']/datatable-body-cell[1]/div/p</value>
       </entry>
@@ -18,6 +22,6 @@
       <name>xpath</name>
       <type>Main</type>
       <value>//*[@class = 'datatable-row-center datatable-row-group ng-star-inserted']/datatable-body-cell[1]/div/p</value>
-      <webElementGuid>ed853386-f1ff-4626-9098-d697ad88f34d</webElementGuid>
+      <webElementGuid>0cd99800-9e30-42e2-bf93-1df9a112e234</webElementGuid>
    </webElementProperties>
 </WebElementEntity>
