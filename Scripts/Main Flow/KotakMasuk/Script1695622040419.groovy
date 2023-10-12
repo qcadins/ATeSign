1f1bdd3727feb9888d441603bf32a55daf502c47
kevin.edgar <kevin.edgar@09-05-0023-0220.ad-ins.com>
2023-10-12 17:34:15 +0700

    Push TIKET get sign link

--- Scripts/Main Flow/KotakMasuk/Script1695622040419.groovy
+++ Scripts/Main Flow/KotakMasuk/Script1695622040419.groovy
@@ -290,6 +290,9 @@ for (int t = 0; t < resultHashMap.keySet().size(); t++) {
 			
             c = 0
         }
+		
+		'get row pada beranda'
+		variable = DriverFactory.webDriver.findElements(By.cssSelector('body > app-root > app-full-layout > div > div.main-panel > div > div.content-wrapper > app-dashboard1 > div:nth-child(3) > div > div > div.card-content > div > app-msx-datatable > section > ngx-datatable > div > datatable-body datatable-row-wrapper'))
         
         'index row distart dari 2 karena yang 1 adalah button ttd'
         indexRow = 2
@@ -298,6 +301,11 @@ for (int t = 0; t < resultHashMap.keySet().size(); t++) {
         modifyObjectTextRefNum = WebUI.modifyObjectProperty(findTestObject('KotakMasuk/text_refnum'), 'xpath', 'equals', 
             ((('/html/body/app-root/app-full-layout/div/div[2]/div/div[2]/app-dashboard1/div[3]/div/div/div[2]/div/app-msx-datatable/section/ngx-datatable/div/datatable-body/datatable-selection/datatable-scroller/datatable-row-wrapper[' + 
             (variable.size() - c)) + ']/datatable-body-row/div[2]/datatable-body-cell[') + indexRow++) + ']/div', true)
+		
+		println indexRow
+		println variable.size()
+		println c
+		WebUI.delay(10)
 		
         'Mengambil teks refnum'
         labelRefNum = WebUI.getText(modifyObjectTextRefNum)
@@ -503,23 +511,7 @@ for (int t = 0; t < resultHashMap.keySet().size(); t++) {
 				
                 'Klik kembali'
                 WebUI.click(findTestObject('Object Repository/KotakMasuk/btn_backViewDokumen'))
-				
-				'Jika button Lastest dapat diklik'
-				if (WebUI.verifyElementClickable(modifyObjectBtnLastest, FailureHandling.OPTIONAL)) {
-					'Klik button Lastest'
-					WebUI.click(modifyObjectBtnLastest, FailureHandling.OPTIONAL)
-				}
-				
-				'pastikan docs ada di halaman sebelum lastest'
-				if (c+1 >= variable.size()) {
-					
-					'Agar dapat ke Lastest, modifikasi button Lastest pada paging'
-					modifyObjectBtnPrevious = WebUI.modifyObjectProperty(findTestObject('Object Repository/KotakMasuk/Sign/btn_Lastest'),
-						'xpath', 'equals', '/html/body/app-root/app-full-layout/div/div[2]/div/div[2]/app-dashboard1/div[3]/div/div/div[2]/div/app-msx-datatable/section/ngx-datatable/div/datatable-footer/div/datatable-pager/ul/li[2]/a/i',
-						true)
-		
-					WebUI.click(modifyObjectBtnPrevious)
-				}
+
             } else {
                 CustomKeywords.'customizekeyword.WriteExcel.writeToExcelStatusReason'(sheet, GlobalVariable.NumofColm, GlobalVariable.StatusFailed, 
                     ((findTestData(excelPathFESignDocument).getValue(GlobalVariable.NumofColm, rowExcel('Reason Failed')) + 
