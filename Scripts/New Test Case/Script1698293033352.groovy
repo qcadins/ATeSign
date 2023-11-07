f962378f13f9db7e10b190c985050711046dd87e
fendy.tio <fendy.tio@09-05-0040-0222.ad-ins.com>
2023-11-07 14:17:02 +0700

    test workflow

--- Scripts/New Test Case/Script1698293033352.groovy
+++ Scripts/New Test Case/Script1698293033352.groovy
@@ -1,39 +1,14 @@
-public class MatrixExample {
-    public static void main(String[] args) {
-        // Define two matrices
-        int[][] matrix1 = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
-        };
+function calculateSum(a, b) {
+  // Deliberate error: Missing semicolon
+  return a + b
+}
 
-        int[][] matrix2 = {
-            {9, 8, 7},
-            {6, 5, 4},
-            {3, 2, 1}
-        };
+console.log("The sum is: " + calculateSum(3, 5))
 
-        // Check if the matrices have the same dimensions for addition
-        if (matrix1.length != matrix2.length || matrix1[0].length != matrix2[0].length) {
-            System.out.println("Matrix addition is not possible due to different dimensions.");
-            return;
-        }
+// Deliberate error: Using undeclared variable
+console.log("The product is: " + product(2, 4))
 
-        // Create a result matrix for addition
-        int[][] resultMatrix = new int[matrix1.length][matrix1[0].length];
-
-        // Perform matrix addition
-        for (int i = 0; i < matrix1.length; i++) {
-            for (int j = 0; j < matrix1[0].length; j++) {
-                resultMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
-            }
-        }
-
-        // Display the result matrix
-        System.out.println("Result Matrix after addition:");
-        for (int i = 0; i < resultMatrix.length; i++) {
-            for (int j = 0; j < resultMatrix[0].length; j++) {
-                System.out.print(resultMatrix[i][j] + " ");
-            }
-            System.out.println();
-
+// Deliberate error: Missing 'else' in the if statement
+if (true) {
+  console.log("This is true")
+}
