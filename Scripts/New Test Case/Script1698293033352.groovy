873816f2548ec8fa67834eb335685de6b2b67fa5
fendy.tio <fendy.tio@09-05-0040-0222.ad-ins.com>
2023-11-07 14:01:59 +0700

    test workflow

---
+++ Scripts/New Test Case/Script1698293033352.groovy
@@ -0,0 +1,15 @@
+
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+println(CustomKeywords.'customizekeyword.GetEmail.getEmailContent'('USERCJFI@OUTLOOK.COM', 'P@ssw0rd123!', 'OTP'))
+
+WebUI.acceptAlert()
+
+WebUI.acceptAlert()
+
+WebUI.acceptAlert()
+
+WebUI.acceptAlert()
+
