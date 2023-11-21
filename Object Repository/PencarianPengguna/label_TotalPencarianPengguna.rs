6d20c7af7b6b06673ff61c9a00e3a73233661ac1
wiky.hendra <wiky.hendra@09-05-0045-0123.ad-ins.com>
2023-11-21 15:26:09 +0700

    AT- Bug Fixing SIT

---
+++ Object Repository/PencarianPengguna/label_TotalPencarianPengguna.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>label_TotalPencarianPengguna</name>
+   <tag></tag>
+   <elementGuidId>c4b8f01a-16cb-46ac-99c3-53cc5c46dc49</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@class = 'datatable-footer ng-star-inserted']/div/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@class = 'datatable-footer ng-star-inserted']/div/div</value>
+      <webElementGuid>0eadcb37-984d-49e7-902a-1d4d7a90b9ca</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
