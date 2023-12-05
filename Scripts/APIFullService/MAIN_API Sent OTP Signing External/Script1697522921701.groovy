946c2bd121ab7348f5d46bdb758a0db4db810f1b
qcadins <qcadins@gmail.com>
2023-12-05 14:06:38 +0700

    api sent otp external

--- Scripts/APIFullService/MAIN_API Sent OTP Signing External/Script1697522921701.groovy
+++ Scripts/APIFullService/MAIN_API Sent OTP Signing External/Script1697522921701.groovy
@@ -119,20 +119,6 @@ for (GlobalVariable.NumofColm = 2; GlobalVariable.NumofColm <= countColmExcel; (
 			
             'jika codenya 0'
             if (code == 0) {
-				
-				'ambil lama waktu yang diperlukan hingga request menerima balikan'
-				def elapsedTime = (respon.getElapsedTime() / 1000) + ' second'
-	
-				'ambil body dari hasil respons'
-				responseBody = respon.getResponseBodyContent()
-	
-				'panggil keyword untuk proses beautify dari respon json yang didapat'
-				CustomKeywords.'customizekeyword.BeautifyJson.process'(responseBody, sheet, rowExcel('Respons') - 1, findTestData(
-						excelPathAPISentOTPSigning).getValue(GlobalVariable.NumofColm, rowExcel('Scenario')))
-	
-				'write to excel response elapsed time'
-				CustomKeywords.'customizekeyword.WriteExcel.writeToExcel'(GlobalVariable.DataFilePath, sheet, rowExcel('Process Time') -
-					1, GlobalVariable.NumofColm - 1, elapsedTime.toString())
 				if(findTestData(excelPathAPISentOTPSigning).getValue(GlobalVariable.NumofColm, rowExcel('Setting Sent OTP by Email (Sign External)')) == '0') {
 					'jika psre bukan privy, lakukan pengecekan store DB'
 					if (GlobalVariable.Psre != 'PRIVY') {
