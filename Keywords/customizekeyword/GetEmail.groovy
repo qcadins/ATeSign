6f93be84d1243ac36de88570e73c52cdc388739b
qcadins <qcadins@gmail.com>
2023-12-28 16:51:08 +0700

    codacy fix

--- Keywords/customizekeyword/GetEmail.groovy
+++ Keywords/customizekeyword/GetEmail.groovy
@@ -1,48 +1,25 @@
 package customizekeyword
 
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-
 import com.kms.katalon.core.annotation.Keyword
-import com.kms.katalon.core.checkpoint.Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling
-import com.kms.katalon.core.testcase.TestCase
-import com.kms.katalon.core.testdata.TestData
-import com.kms.katalon.core.testobject.TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import com.sun.mail.pop3.POP3Store
-import javax.mail.*
 import javax.mail.BodyPart as BodyPart
 import javax.mail.Folder as Folder
 import javax.mail.Message
-import javax.mail.MessagingException
-import javax.mail.NoSuchProviderException
 import javax.mail.Session as Session
 import javax.mail.Store
-import javax.mail.internet.InternetAddress
 import javax.mail.internet.MimeMultipart as MimeMultipart
 import org.jsoup.Jsoup as Jsoup
 import org.jsoup.nodes.Document as Document
 import java.util.regex.Matcher as Matcher
 import java.util.regex.Pattern as Pattern
 
-import internal.GlobalVariable
-
 public class GetEmail {
 
 	@Keyword
 	getEmailContent(String email, String inputPassword, String value) {
 		String host = 'outlook.office365.com'
-		
+
 		String otpCode
-		
+
 		String username = email
 
 		String password = inputPassword
@@ -68,23 +45,23 @@ public class GetEmail {
 		emailFolder.open(Folder.READ_ONLY)
 
 		// retrieve the messages from the folder in an array and print it
-		Message[] messages = emailFolder.getMessages()
+		Message[] messages = emailFolder.messages
 
-		System.out.println('messages.length---' + messages.length)
+		println('messages.length---' + messages.length)
 
 		int lastEmailSequence = messages.length
 
 		Message message = messages[(lastEmailSequence - 1)]
 
-		String emailSubject = message.getSubject()
+		String emailSubject = message.subject
 
-		System.out.println('Email Number ' + lastEmailSequence)
+		println('Email Number ' + lastEmailSequence)
 
-		System.out.println('Subject: ' + emailSubject)
+		println('Subject: ' + emailSubject)
 
-		System.out.println('From: ' + (message.getFrom()[0]))
+		println('From: ' + (message.from[0]))
 
-		Object content = message.getContent()
+		Object content = message.content
 
 		// Pastikan content adalah instance dari MimeMultipart
 		if (content instanceof MimeMultipart) {
@@ -93,17 +70,17 @@ public class GetEmail {
 			StringBuilder bodyText = new StringBuilder()
 
 			String textWithoutHtml
-			
+
 			// Loop melalui semua bagian dalam MimeMultipart
-			for (int i = 0; i < mimeMultipart.getCount(); i++) {
+			for (int i = 0 ; i < mimeMultipart.count ; i++) {
 				BodyPart bodyPart = mimeMultipart.getBodyPart(i)
 
-				String contentType = bodyPart.getContentType()
+				String contentType = bodyPart.contentType
 				Document doc
 
 				// Hanya ambil bagian teks dari bagian yang berisi teks
 				if (contentType.startsWith('text/plain') || contentType.startsWith('text/html')) {
-					String partText = ((bodyPart.getContent()) as String)
+					String partText = ((bodyPart.content) as String)
 
 					// Gunakan jsoup untuk menghilangkan elemen HTML
 					doc = Jsoup.parse(partText)
@@ -111,27 +88,27 @@ public class GetEmail {
 					textWithoutHtml = doc.text()
 
 					println(textWithoutHtml)
-					
+
 					break
 				} else if(!(contentType.startsWith('text/plain') || contentType.startsWith('text/html'))) {
-					InputStream is = bodyPart.getInputStream();
-					BufferedReader br = new BufferedReader(new InputStreamReader(is));
-					String line;
+					InputStream is = bodyPart.inputStream
+					BufferedReader br = new BufferedReader(new InputStreamReader(is))
+					String line
 					while ((line = br.readLine()) != null) {
-						bodyText.append(line);
+						bodyText.append(line)
 					}
 
 					// Gunakan jsoup untuk menghilangkan elemen HTML
 					doc = Jsoup.parse(bodyText.toString())
 
 					textWithoutHtml = findText(doc.text())
-					
+
 					break
 				}
 			}
 
 			otpCode = findOtpCode(textWithoutHtml.toString())
-			
+
 			println('OTP Code: ' + otpCode)
 		}
 
@@ -159,19 +136,20 @@ public class GetEmail {
 	}
 
 	def findText(String text) {
-		Pattern pattern = Pattern.compile("Yth(.+?)Tim eSignHub");
+		Pattern pattern = Pattern.compile("Yth(.+?)Tim eSignHub")
 
-		Matcher matcher = pattern.matcher(text);
+		Matcher matcher = pattern.matcher(text)
 
 		if (matcher.find()) {
 			// Extract the matched text
-			String extractedText = matcher.group(1).trim();
-			String cleanedText = extractedText.replaceAll("[\\n]+", " ");
+			String extractedText = matcher.group(1).trim()
+			String cleanedText = extractedText.replaceAll("[\\n]+", " ")
 			return extractedText
 		} else {
-			System.out.println("Match not found.");
-			
+			println("Match not found.")
+
 			return 'false'
 		}
 	}
 }
+
