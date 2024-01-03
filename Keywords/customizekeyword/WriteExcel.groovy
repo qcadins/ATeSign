ea0b12278f47c52e2aac8452eb2039ecd02d7761
qcadins <qcadins@gmail.com>
2024-01-03 11:32:36 +0700

    codacy fixing

--- Keywords/customizekeyword/WriteExcel.groovy
+++ Keywords/customizekeyword/WriteExcel.groovy
@@ -7,15 +7,16 @@ import internal.GlobalVariable
 import org.apache.poi.xssf.usermodel.XSSFRow
 
 public class WriteExcel {
+	
 	@Keyword
 	writeToExcel(String filePath, String sheetName, int rowNo, int collNo, String cellValue) {
-		FileInputStream file = new FileInputStream (new File(filePath))
+		FileInputStream file = new FileInputStream(new File(filePath))
 
 		XSSFWorkbook workbook = new XSSFWorkbook(file)
 		XSSFSheet sheet = workbook.getSheet(sheetName)
 
-		def row = sheet.getRow(rowNo) ?: sheet.createRow(rowNo)
-		def cell = row.getCell(collNo) ?: row.createCell(collNo)
+		row = sheet.getRow(rowNo) ?: sheet.createRow(rowNo)
+		cell = row.getCell(collNo) ?: row.createCell(collNo)
 
 		cell.setCellValue(cellValue)
 
@@ -44,7 +45,7 @@ public class WriteExcel {
 
 	@Keyword
 	writeToExcelDecimal(String filePath, String sheetName, int rowNo, int collNo, Double cellValue) {
-		FileInputStream file = new FileInputStream (new File(filePath)) //initiate excel repository
+		FileInputStream file = new FileInputStream(new File(filePath)) //initiate excel repository
 
 		XSSFWorkbook workbook = new XSSFWorkbook(file)
 		XSSFSheet sheet = workbook.getSheet(sheetName)
@@ -70,7 +71,7 @@ public class WriteExcel {
 
 	@Keyword
 	writeToExcelFormula(String filePath, String sheetName, int rowNo, int collNo, String cellValue) throws IOException {
-		FileInputStream file = new FileInputStream (new File(filePath))
+		FileInputStream file = new FileInputStream(new File(filePath))
 		XSSFWorkbook workbook = new XSSFWorkbook(file)
 		XSSFSheet sheet = workbook.getSheet(sheetName)
 
@@ -96,23 +97,24 @@ public class WriteExcel {
 	//keyword getExcelRow
 	@Keyword
 	public int getExcelRow(String filePath, String sheetName, String cellValue) {
-		FileInputStream file = new FileInputStream (new File(filePath)) //initiate excel repository
-		XSSFWorkbook workbook = new XSSFWorkbook(file);
-		XSSFSheet sheet = workbook.getSheet(sheetName); //getSheet -> sheet num n (start from index 0)
-		XSSFRow row = null;
-		int row_num=-1;
-		for(int i=0; i <= sheet.getLastRowNum(); i++) {
+		FileInputStream file = new FileInputStream(new File(filePath)) //initiate excel repository
+		XSSFWorkbook workbook = new XSSFWorkbook(file)
+		XSSFSheet sheet = workbook.getSheet(sheetName) //getSheet -> sheet num n (start from index 0)
+		XSSFRow row = null
+		int rowNum = -1
+		for (int i = 0; i <= sheet.lastRowNum; i++) {
 			row = sheet.getRow(i)
-			try{
-				if(row.getCell(0).getStringCellValue().equalsIgnoreCase(cellValue)){
-					row_num = i
+			try {
+				if (row.getCell(0).stringCellValue.equalsIgnoreCase(cellValue)) {
+					rowNum = i
 					break
 				}
 			}
-			catch(Exception e){
+			catch(Exception e) {
 			}
 		}
 		file.close()
-		return row_num+1
+		rowNum + 1
 	}
+	
 }
