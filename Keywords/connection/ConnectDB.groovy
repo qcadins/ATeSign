1ed38b123f9348ebdba45ddcb1e40d7f2b62ae0a
qcadins <qcadins@gmail.com>
2024-01-03 15:36:05 +0700

    codacy fixing

--- Keywords/connection/ConnectDB.groovy
+++ Keywords/connection/ConnectDB.groovy
@@ -1,21 +1,18 @@
 package connection
 
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import java.sql.Connection
 import java.sql.DriverManager
 import com.kms.katalon.core.annotation.Keyword
 
 public class ConnectDB {
 
     @Keyword
-    connectDBeSign() {
-        def testData = findTestData('Login/Login')
-        
-        String servername = testData.getValue(1, 13)
-        String port = testData.getValue(2, 13)
-        String database = testData.getValue(3, 13)
-        String username = testData.getValue(4, 13)
-        String password = testData.getValue(5, 13)
+    connectDBeSign() {        
+        String servername = findTestData('Login/Login').getValue(1, 13)
+        String port = findTestData('Login/Login').getValue(2, 13)
+        String database = findTestData('Login/Login').getValue(3, 13)
+        String username = findTestData('Login/Login').getValue(4, 13)
+        String password = findTestData('Login/Login').getValue(5, 13)
 
         String url = "${servername}:${port}/${database}"
 
