8947712ddb74c4a6525a3d167fb5a715dd54a113
qcadins <qcadins@gmail.com>
2024-01-05 14:12:55 +0700

    codacy fixing

---
+++ Object Repository/Postman/verifyLivenessFaceCompare Embed.rs
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>verifyLivenessFaceCompare Embed</name>
+   <tag></tag>
+   <elementGuidId>368a23fd-559d-460a-95f6-87b570cf864b</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <authorizationRequest>
+      <authorizationInfo>
+         <entry>
+            <key>bearerToken</key>
+            <value>${token}</value>
+         </entry>
+      </authorizationInfo>
+      <authorizationType>Bearer</authorizationType>
+   </authorizationRequest>
+   <autoUpdateContent>false</autoUpdateContent>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n    \&quot;audit\&quot;: {\n        \&quot;callerId\&quot;: \&quot;${callerId}\&quot;\n    },\n  \t\&quot;msg\&quot;: \&quot;${msg}\&quot;,\n    \&quot;tenantCode\&quot;: \&quot;${tenantCode}\&quot;,\n  \t\&quot;vendorCode\&quot;: \&quot;${vendorCode}\&quot;,\n    \&quot;img1\&quot;: \&quot;${img}\&quot;\n}&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+      <webElementGuid>5f65da2e-60a2-4ccd-9663-7a1ad9368e40</webElementGuid>
+   </httpHeaderProperties>
+   <katalonVersion>8.5.5</katalonVersion>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>${base_url}/services/embed/user/verifyLivenessFaceCompareEmbed</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <variables>
+      <defaultValue>GlobalVariable.base_url</defaultValue>
+      <description></description>
+      <id>27d10953-943b-408f-9425-658b2697484a</id>
+      <masked>false</masked>
+      <name>base_url</name>
+   </variables>
+   <variables>
+      <defaultValue>GlobalVariable.token</defaultValue>
+      <description></description>
+      <id>25297977-6c59-4bba-915d-cb830a61d574</id>
+      <masked>false</masked>
+      <name>token</name>
+   </variables>
+   <variables>
+      <defaultValue>GlobalVariable.Tenant</defaultValue>
+      <description></description>
+      <id>bb90d177-b89a-4582-89cd-90c84c82893d</id>
+      <masked>false</masked>
+      <name>tenantCode</name>
+   </variables>
+   <variables>
+      <defaultValue>GlobalVariable.Psre</defaultValue>
+      <description></description>
+      <id>3608ace3-dd8b-4ea5-9322-83fdd310f7e8</id>
+      <masked>false</masked>
+      <name>vendorCode</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
--- Scripts/Other API/MAIN_API Verify Liveness Face Compare Embed/Script1704424057354.groovy
+++ Scripts/Other API/MAIN_API Verify Liveness Face Compare Embed/Script1704424057354.groovy
@@ -1,3 +1,4 @@
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
@@ -5,18 +6,19 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import java.nio.charset.StandardCharsets as StandardCharsets
 import java.sql.Connection as Connection
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
+import java.text.SimpleDateFormat as SimpleDateFormat
 import internal.GlobalVariable as GlobalVariable
-
-'connect DB eSign'
-Connection conneSign = CustomKeywords.'connection.ConnectDB.connectDBeSign'()
 
 'get data file path'
 GlobalVariable.DataFilePath = CustomKeywords.'customizekeyword.WriteExcel.getExcelPath'('\\Excel\\2.1 Esign - API Only.xlsx')
 
+'connect DB eSign'
+Connection conneSign = CustomKeywords.'connection.ConnectDB.connectDBeSign'()
+
 'get colm excel'
 int countColmExcel = findTestData(excelPath).columnNumbers
+
+String selfPhoto
 
 for (GlobalVariable.NumofColm = 2; GlobalVariable.NumofColm <= countColmExcel; (GlobalVariable.NumofColm)++) {
     if (findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Status')).length() == 0) {
@@ -36,22 +38,38 @@ for (GlobalVariable.NumofColm = 2; GlobalVariable.NumofColm <= countColmExcel; (
             GlobalVariable.Tenant = findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Tenant Login'))
         }
         
+        'check if tidak mau menggunakan vendor code yang benar'
+        if (findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('use Correct Vendor Code')) == 'No') {
+            'set vendor kosong'
+            GlobalVariable.Psre = findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Wrong Vendor Code'))
+        } else if (findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('use Correct Vendor Code')) == 'Yes') {
+            'get vendor per case dari colm excel'
+            GlobalVariable.Psre = findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Vendor Login'))
+        }
+        
+        'check if self photo mau menggunakan base64 yang salah atau benar'
+        if (findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('enter Correct base64 SelfPhoto')) == 'Yes') {
+            selfPhoto = CustomKeywords.'customizekeyword.ConvertFile.base64File'(findTestData(excelPath).getValue(GlobalVariable.NumofColm, 
+                    rowExcel('selfPhoto')))
+        } else if (findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('enter Correct base64 SelfPhoto')) == 
+        'No') {
+            selfPhoto = findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('selfPhoto'))
+        }
+        
         'get aesKet Tenant'
         aesKey = CustomKeywords.'connection.APIFullService.getAesKeyBasedOnTenant'(conneSign, GlobalVariable.Tenant)
 
-        currentDate = LocalDateTime.now()
+        currentDate = new Date()
 
-		localeIndonesia = new Locale("id", "ID")
-		
-		formatter = DateTimeFormatter.ofPattern('yyyy-MM-dd HH:mm:ss', localeIndonesia)
-		
-		formattedDate = currentDate.format(formatter)
+        dateFormat = new SimpleDateFormat('yyyy-MM-dd HH:mm:ss')
+
+        timestamp = dateFormat.format(currentDate)
 
         if (aesKey.toString() != 'null') {
             'pembuatan message yang akan dienkrip'
             msg = (((((('{\'officeCode\':\'' + findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('officeCode'))) + 
             '\',\'email\':\'') + findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('email'))) + '\',\'timestamp\':\'') + 
-            formattedDate) + '\'}')
+            timestamp) + '\'}')
 
             if (findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Use Correct Msg')) == 'No') {
                 'officecode + email + time stamp tanpa encrypt'
@@ -65,82 +83,42 @@ for (GlobalVariable.NumofColm = 2; GlobalVariable.NumofColm <= countColmExcel; (
         }
         
         'HIT API'
-        respon = WS.sendRequest(findTestObject('Postman/RegionList Embed', [
-				('callerId') : findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('callerId')), 
-				('msg') : endcodedMsg]))
+        respon = WS.sendRequest(findTestObject('Postman/verifyLivenessFaceCompare Embed', [
+					('callerId') : findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('callerId')), 
+					('msg') : endcodedMsg, 
+					('img') : selfPhoto]))
+
+        'ambil lama waktu yang diperlukan hingga request menerima balikan'
+        elapsedTime = ((respon.elapsedTime / 1000) + ' second')
+
+        'ambil body dari hasil respons'
+        responseBody = respon.responseBodyContent
+
+        'panggil keyword untuk proses beautify dari respon json yang didapat'
+        CustomKeywords.'customizekeyword.BeautifyJson.process'(responseBody, sheet, rowExcel('Respons') - 1, findTestData(
+                excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Scenario')))
+
+        'write to excel response elapsed time'
+        CustomKeywords.'customizekeyword.WriteExcel.writeToExcel'(GlobalVariable.DataFilePath, sheet, rowExcel('Process Time') - 
+            1, GlobalVariable.NumofColm - 1, elapsedTime.toString())
 
         'Jika status HIT API 200 OK'
         if (WS.verifyResponseStatusCode(respon, 200, FailureHandling.OPTIONAL) == true) {
             'mengambil status code berdasarkan response HIT API'
             statusCode = WS.getElementPropertyValue(respon, 'status.code', FailureHandling.OPTIONAL)
 
-			'ambil lama waktu yang diperlukan hingga request menerima balikan'
-			elapsedTime = (respon.elapsedTime / 1000) + ' second'
-	
-			'ambil body dari hasil respons'
-			responseBody = respon.responseBodyContent
-	
-			'panggil keyword untuk proses beautify dari respon json yang didapat'
-			CustomKeywords.'customizekeyword.BeautifyJson.process'(responseBody, sheet, rowExcel('Respons') - 1, findTestData(
-					excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Scenario')))
-	
-			'write to excel response elapsed time'
-			CustomKeywords.'customizekeyword.WriteExcel.writeToExcel'(GlobalVariable.DataFilePath, sheet, rowExcel('Process Time') -
-				1, GlobalVariable.NumofColm - 1, elapsedTime.toString())
-			
             'jika status codenya 0'
             if (statusCode == 0) {
-                resultRegionCode = WS.getElementPropertyValue(respon, 'regionList.regionCode', FailureHandling.OPTIONAL)
-
-                resultRegionName = WS.getElementPropertyValue(respon, 'regionList.regionName', FailureHandling.OPTIONAL)
-
-                if (GlobalVariable.checkStoreDB == 'Yes') {
-                    'declare arraylist arraymatch'
-                    ArrayList arrayMatch = []
-
-                    'get data store db'
-                    ArrayList result = CustomKeywords.'connection.APIFullService.getRegionList'(conneSign)
-
-                    'declare arrayindex'
-                    arrayindex = 0
-
-                    for (index = 0; index < (result.size() / 2); index++) {
-                        'encrypt region code'
-                        encryptMsg = CustomKeywords.'customizekeyword.ParseText.parseEncrypt'(result[arrayindex++], aesKey)
-
-                        'verify region code'
-                        arrayMatch.add(WebUI.verifyMatch(encryptMsg, resultRegionCode[index], false, FailureHandling.CONTINUE_ON_FAILURE))
-
-                        'verify region name'
-                        arrayMatch.add(WebUI.verifyMatch(result[arrayindex++], resultRegionName[index], false, FailureHandling.CONTINUE_ON_FAILURE))
-                    }
-                    
-                    'jika data db tidak sesuai dengan excel'
-                    if (arrayMatch.contains(false)) {
-                        GlobalVariable.FlagFailed = 1
-
-                        'Write To Excel GlobalVariable.StatusFailed and GlobalVariable.ReasonFailedStoredDB'
-                        CustomKeywords.'customizekeyword.WriteExcel.writeToExcelStatusReason'(sheet, GlobalVariable.NumofColm, 
-                            GlobalVariable.StatusFailed, (findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel(
-                                    'Reason Failed')) + ';') + GlobalVariable.ReasonFailedStoredDB)
-                    }
-                }
-                
-                'tulis sukses jika store DB berhasil'
-                if (GlobalVariable.FlagFailed == 0) {
-                    'write to excel success'
-                    CustomKeywords.'customizekeyword.WriteExcel.writeToExcel'(GlobalVariable.DataFilePath, sheet, 0, GlobalVariable.NumofColm - 
-                        1, GlobalVariable.StatusSuccess)
-                }
+                'write to excel success'
+                CustomKeywords.'customizekeyword.WriteExcel.writeToExcel'(GlobalVariable.DataFilePath, sheet, 0, GlobalVariable.NumofColm - 
+                    1, GlobalVariable.StatusSuccess)
             } else {
                 'call function get API error message'
                 getErrorMessageAPI(respon)
             }
         } else {
-            'write to excel status failed dan reason : '
-            CustomKeywords.'customizekeyword.WriteExcel.writeToExcelStatusReason'(sheet, GlobalVariable.NumofColm, GlobalVariable.StatusFailed, 
-                (findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Reason Failed')).replace('-', '') + 
-                ';') + GlobalVariable.ReasonFailedHitAPI)
+            'call function get API error message'
+            getErrorMessageAPI(respon)
         }
     }
 }
@@ -171,7 +149,7 @@ def encryptEncodeValue(String value, String aesKey) {
         return URLEncoder.encode(encryptMsg, StandardCharsets.UTF_8.toString())
     }
     catch (UnsupportedEncodingException ex) {
-        new RuntimeException(ex.cause)
+        throw new RuntimeException(ex.getCause())
     } 
 }
 
