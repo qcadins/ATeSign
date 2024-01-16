19af025d5bd0d77447ad25124189c91ba1ba4958
kevin.edgar <kevin.edgar@09-05-0023-0220.ad-ins.com>
2024-01-16 10:39:20 +0700

    push AT get balmut get baltype

---
+++ Object Repository/APIFullService/Postman/Get List Balance Type.rs
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>Get List Balance Type</name>
+   <tag></tag>
+   <elementGuidId>6f4c5ebb-4b1c-4d1b-a4b5-e62aba486d89</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <autoUpdateContent>false</autoUpdateContent>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n    \&quot;audit\&quot;: {\n        \&quot;callerId\&quot;: \&quot;${callerId}\&quot;\n    }\n}&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+      <webElementGuid>55a3c79a-658a-4dab-8ed0-8617f60e752f</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>x-api-key</name>
+      <type>Main</type>
+      <value>${apiKey}@${tenantCode}</value>
+      <webElementGuid>18239c47-e0d7-4974-b025-1d36da5981d0</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Cookie</name>
+      <type>Main</type>
+      <value>idjs=E0FA5E07F126108F89E0202AB001F340</value>
+      <webElementGuid>d548ba2e-ee4e-4770-b43b-4f18408eb309</webElementGuid>
+   </httpHeaderProperties>
+   <katalonVersion>9.0.0</katalonVersion>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>${base_url}/services/external/data/getListBalanceType</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <variables>
+      <defaultValue>GlobalVariable.api_key</defaultValue>
+      <description></description>
+      <id>1b3adf70-3808-4a89-9019-a25dacc7c654</id>
+      <masked>false</masked>
+      <name>apiKey</name>
+   </variables>
+   <variables>
+      <defaultValue>GlobalVariable.Tenant</defaultValue>
+      <description></description>
+      <id>7c93e10b-a9b0-45c7-8854-b6df89f25e20</id>
+      <masked>false</masked>
+      <name>tenantCode</name>
+   </variables>
+   <variables>
+      <defaultValue>GlobalVariable.base_url</defaultValue>
+      <description></description>
+      <id>516dc134-0971-4e69-84f2-90b317949d32</id>
+      <masked>false</masked>
+      <name>base_url</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Scripts/APIFullService/MAIN_API Get Balance Mutation External/Script1705285472896.groovy
@@ -0,0 +1,170 @@
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import java.sql.Connection as Connection
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import groovy.json.JsonSlurper as JsonSlurper
+
+'get data file path'
+GlobalVariable.DataFilePath = CustomKeywords.'customizekeyword.WriteExcel.getExcelPath'('\\Excel\\2.1 Esign - Full API Services.xlsx')
+
+'connect DB eSign'
+Connection conneSign = CustomKeywords.'connection.ConnectDB.connectDBeSign'()
+
+'get colm excel'
+int countColmExcel = findTestData(excelPath).columnNumbers
+
+'looping API Download Document'
+for (GlobalVariable.NumofColm = 2; GlobalVariable.NumofColm <= countColmExcel; (GlobalVariable.NumofColm)++) {
+    if (findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Status')).length() == 0) {
+        break
+    } else if (findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Status')).equalsIgnoreCase(
+        'Unexecuted')) {
+        'setting menggunakan base url yang benar atau salah'
+        CustomKeywords.'connection.APIFullService.settingBaseUrl'(excelPath, GlobalVariable.NumofColm, rowExcel(
+                'Use Correct Base Url'))
+
+        'check if tidak mau menggunakan tenant code yang benar'
+        if (findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Use Correct Tenant Code')) == 
+        'No') {
+            'set tenant kosong'
+            GlobalVariable.Tenant = findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Wrong Tenant Code'))
+        } else if (findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Use Correct Tenant Code')) == 
+        'Yes') {
+            GlobalVariable.Tenant = findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Tenant Login'))
+        }
+        
+        'check if mau menggunakan api_key yang salah atau benar'
+        if (findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Use Correct API Key')) == 'Yes') {
+            'get api key dari db'
+            GlobalVariable.api_key = CustomKeywords.'connection.APIFullService.getTenantAPIKey'(conneSign, GlobalVariable.Tenant)
+        } else if (findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Use Correct API Key')) == 
+        'No') {
+            'get api key salah dari excel'
+            GlobalVariable.api_key = findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Wrong API Key'))
+        }
+        
+        'HIT API get template sign loc'
+        respon = WS.sendRequest(findTestObject('APIFullService/Postman/Get Balance Mutation', [
+			('callerId') : findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('callerId')),
+			('balType') : findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('$balanceType')),
+			('startDate') : findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('$Start Date')),
+			('endDate') : findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('$End Date')),
+			('offCode') : findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Office Code')),
+			('businessLine') : findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Business Line Code')),
+			('regionCode') : findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Region Code')),]))
+
+        'ambil lama waktu yang diperlukan hingga request menerima balikan'
+        elapsedTime = ((respon.elapsedTime / 1000) + ' second')
+
+        'ambil body dari hasil respons'
+        responseBody = respon.responseBodyContent
+
+        'Jika status HIT API 200 OK'
+        if (WS.verifyResponseStatusCode(respon, 200, FailureHandling.OPTIONAL) == true) {
+            code = WS.getElementPropertyValue(respon, 'status.code', FailureHandling.OPTIONAL)
+			
+			'panggil keyword untuk proses beautify dari respon json yang didapat'
+			CustomKeywords.'customizekeyword.BeautifyJson.process'(responseBody, sheet, rowExcel('Respons') - 1, findTestData(
+					excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Scenario')))
+	
+			'write to excel response elapsed time'
+			CustomKeywords.'customizekeyword.WriteExcel.writeToExcel'(GlobalVariable.DataFilePath, sheet, rowExcel('Process Time') -
+				1, GlobalVariable.NumofColm - 1, elapsedTime.toString())
+
+            if (code == 0) {
+                if ((GlobalVariable.checkStoreDB == 'Yes') && (GlobalVariable.FlagFailed == 0)) {
+                    'declare arraylist arraymatch'
+                    ArrayList arrayMatch = []
+
+                    'get data store db'
+                    ArrayList result = CustomKeywords.'connection.APIFullService.getBalanceMutation'(conneSign,
+						findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('$balanceType')),
+						findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('$Start Date')),
+						findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('$End Date')),
+						findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Office Code')),
+						findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Business Line Code')),
+						findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('Region Code')))
+					
+					String resultchange = result.toString().replace('[', '').replace(']', '').replace(' ' , '')
+					
+					result = resultchange.split(',', -1)
+                    
+					'declare arrayindex'
+                    arrayindex = 0
+					
+					int size = 0, module = 0
+
+					'cek apakah merupakan stamp duty query'
+					if (findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('$balanceType')) == 'SDT') {
+						size = result.size() / 19
+					} else {
+						size = result.size() / 11
+					}
+								
+					'looping semua array yang terdapat di respon API dan DB'
+					for (i = 0; i < size; i++) {
+						String a = WS.getElementPropertyValue(respon, 'balanceMutations[' + i + ']', FailureHandling.OPTIONAL)
+						
+						a = a.replace('[', '').replace(']', '').replace(' ' , '')
+						
+						ArrayList apiResult = a.split(',', -1)
+						
+						for (int j = 0; j < apiResult.size(); j++) {
+							'verify no urutan'
+							arrayMatch.add(WebUI.verifyMatch(apiResult[j], result[arrayindex++], false, FailureHandling.CONTINUE_ON_FAILURE))
+							
+							if (findTestData(excelPath).getValue(GlobalVariable.NumofColm, rowExcel('$balanceType')) == 'SDT') {
+								module = arrayindex % 19
+							} else {
+								module = arrayindex % 11
+							}
+							
+							'jika next array index kosong, skip pengecekan ke DB'
+							if ((module) == 1 && (result[arrayindex] == '' || result[arrayindex] == 'null')) {
+								arrayindex += 2
+							} else if (result[arrayindex] == 'null' || result[arrayindex] == '') {
+								arrayindex += 1
+							}
+						}
+					}
+                          
+                    'jika data db tidak sesuai dengan excel'
+                    if (arrayMatch.contains(false)) {
+                        GlobalVariable.FlagFailed = 1
+
+                        'Write To Excel GlobalVariable.StatusFailed and GlobalVariable.ReasonFailedStoredDB'
+                        CustomKeywords.'customizekeyword.WriteExcel.writeToExcelStatusReason'(sheet, GlobalVariable.NumofColm, 
+                            GlobalVariable.StatusFailed, (findTestData(excelPath).getValue(GlobalVariable.NumofColm, 
+                                rowExcel('Reason Failed')) + ';') + GlobalVariable.ReasonFailedStoredDB)
+                    }
+                }
+                'tulis sukses jika store DB berhasil'
+                if (GlobalVariable.FlagFailed == 0) {
+                    'write to excel success'
+                    CustomKeywords.'customizekeyword.WriteExcel.writeToExcel'(GlobalVariable.DataFilePath, sheet, 0, GlobalVariable.NumofColm - 
+                        1, GlobalVariable.StatusSuccess)
+                }
+            } else {
+                getErrorMessageAPI(respon)
+            }
+        } else {
+            getErrorMessageAPI(respon)
+        }
+    }
+}
+
+def rowExcel(String cellValue) {
+    CustomKeywords.'customizekeyword.WriteExcel.getExcelRow'(GlobalVariable.DataFilePath, sheet, cellValue)
+}
+
+def getErrorMessageAPI(def respon) {
+    'mengambil status code berdasarkan response HIT API'
+    message = WS.getElementPropertyValue(respon, 'status.message', FailureHandling.OPTIONAL)
+
+    'Write To Excel GlobalVariable.StatusFailed and errormessage'
+    CustomKeywords.'customizekeyword.WriteExcel.writeToExcelStatusReason'(sheet, GlobalVariable.NumofColm, GlobalVariable.StatusFailed, 
+        ('<' + message) + '>')
+}
