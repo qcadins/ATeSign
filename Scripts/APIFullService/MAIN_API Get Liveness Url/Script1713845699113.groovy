bef084901f7d07ad8fc756955087c1a2f9466ad9
wiky.hendra <wiky.hendra@09-05-0045-0123.ad-ins.com>
2024-04-23 11:47:22 +0700

    ATEENDIGO - 369 Fixing Codacy

--- Scripts/APIFullService/MAIN_API Get Liveness Url/Script1713845699113.groovy
+++ Scripts/APIFullService/MAIN_API Get Liveness Url/Script1713845699113.groovy
@@ -3,7 +3,6 @@ import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.testobject.ResponseObject as ResponseObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import java.sql.Connection as Connection
 import internal.GlobalVariable as GlobalVariable
 
